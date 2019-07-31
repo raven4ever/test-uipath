587f386905541bf7a2a6c643c311d37e020dff45
Adrian <wrtv23@gmail.com>
2019-07-31 22:32:54 +0300

    terraform: traffic manager + alert

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ resource "azurerm_public_ip" "myterraformpublicip" {
   location            = "${lookup(var.vm_configuration[count.index], "location")}"
   resource_group_name = "${var.default_resource_group_name}"
   allocation_method   = "Dynamic"
+  domain_name_label   = "${lookup(var.vm_configuration[count.index], "name")}-${var.domain_name_label}"
 }
 
 # Create network interface
@@ -50,14 +51,15 @@ resource "azurerm_network_interface" "myterraformnic" {
 
 # Create virtual machine
 resource "azurerm_virtual_machine" "myterraformvm" {
-  name                  = "myVM"
-  location              = "westeurope"
+  count                 = length(var.vm_configuration)
+  name                  = "${lookup(var.vm_configuration[count.index], "name")}-VM"
+  location              = "${lookup(var.vm_configuration[count.index], "location")}"
   resource_group_name   = "${var.default_resource_group_name}"
-  network_interface_ids = ["${azurerm_network_interface.myterraformnic[0].id}"]
-  vm_size               = "Standard_D2s_v3"
+  network_interface_ids = ["${azurerm_network_interface.myterraformnic[count.index].id}"]
+  vm_size               = "${lookup(var.vm_configuration[count.index], "vm_size")}"
 
   storage_os_disk {
-    name              = "myOsDisk"
+    name              = "${lookup(var.vm_configuration[count.index], "name")}-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Premium_LRS"
@@ -66,21 +68,69 @@ resource "azurerm_virtual_machine" "myterraformvm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04.0-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
   os_profile {
-    computer_name  = "myvm"
-    admin_username = "azureuser"
+    computer_name  = "${lookup(var.vm_configuration[count.index], "name")}-VM"
+    admin_username = "${var.vm_user}"
   }
 
   os_profile_linux_config {
     disable_password_authentication = true
     ssh_keys {
       path     = "/home/azureuser/.ssh/authorized_keys"
-      key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTVSc3KQYeaW3tnYnFrLMYILMhWXaA0ZBtbtEVM3lTSBfjJ5jhUxuYxASwI2uq9d2gl7WS4zvGKlpijSYj/g1iiNkBseQ+DwqxkN3R2mwNjgASvKEegFgWrHEhnBRYPas8wFrEXcAC2q9Je4AgzD1dybYSEHyfVFJJuLKfUtU8rVd5WhDGx7F0o1hNgGBFpuc9cM5/+WOISZRrgze6rwxvgbGLVw0q6U+vOqT46gE+A1CXpcP1TqcO4EwadQYunzLRs08HNLhLlSbG0hMYrPI+Uu8OdriT7HxjWIRf9FowJBFAxS4uR5TCGMBiSNb/LWxIJLWORu8eIav9rFAaMhDP adrian@adrian-K52Jr"
+      key_data = "${var.ssh_key}"
     }
   }
 }
 
+resource "azurerm_traffic_manager_profile" "netcoreapp" {
+  name                   = "netcoreapp-traffic-manager"
+  resource_group_name    = "${var.default_resource_group_name}"
+  traffic_routing_method = "Priority"
+
+  dns_config {
+    relative_name = "${var.domain_name_label}"
+    ttl           = 100
+  }
+
+  monitor_config {
+    protocol = "http"
+    port     = 80
+    path     = "/"
+  }
+}
+
+resource "azurerm_traffic_manager_endpoint" "endpoints" {
+  count               = length(azurerm_public_ip.myterraformpublicip)
+  name                = "${lookup(var.vm_configuration[count.index], "name")}-endpoint"
+  resource_group_name = "${var.default_resource_group_name}"
+  profile_name        = "${azurerm_traffic_manager_profile.netcoreapp.name}"
+  target_resource_id  = "${azurerm_public_ip.myterraformpublicip[count.index].id}"
+  type                = "azureEndpoints"
+}
+
+resource "azurerm_monitor_metric_alertrule" "trafficmanagermailrule" {
+  name                = "${azurerm_traffic_manager_profile.netcoreapp.name}-primaryendpoint"
+  resource_group_name = "${var.default_resource_group_name}"
+  location            = "${var.default_region}"
+  description         = "An alert rule to send a mail when the primary endpoint is down"
+  enabled             = true
+
+  resource_id = "${azurerm_traffic_manager_endpoint.endpoints[0].id}"
+  metric_name = "Endpoint status by endpoint"
+  operator    = "LessThan"
+  threshold   = 1
+  aggregation = "Average"
+  period      = "PT5M"
+
+  email_action {
+    send_to_service_owners = false
+
+    custom_emails = [
+      "wrtv23@gmail.com"
+    ]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,18 @@
+variable "domain_name_label" {
+  description = "Base DNS label for the public IP addresses"
+  default     = "ansibleterraform"
+}
+
+variable "vm_user" {
+  description = "OS user to be created"
+  default     = "azureuser"
+}
+
+variable "ssh_key" {
+  description = "SSH key to be copied to the machines"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTVSc3KQYeaW3tnYnFrLMYILMhWXaA0ZBtbtEVM3lTSBfjJ5jhUxuYxASwI2uq9d2gl7WS4zvGKlpijSYj/g1iiNkBseQ+DwqxkN3R2mwNjgASvKEegFgWrHEhnBRYPas8wFrEXcAC2q9Je4AgzD1dybYSEHyfVFJJuLKfUtU8rVd5WhDGx7F0o1hNgGBFpuc9cM5/+WOISZRrgze6rwxvgbGLVw0q6U+vOqT46gE+A1CXpcP1TqcO4EwadQYunzLRs08HNLhLlSbG0hMYrPI+Uu8OdriT7HxjWIRf9FowJBFAxS4uR5TCGMBiSNb/LWxIJLWORu8eIav9rFAaMhDP adrian@adrian-K52Jr"
+}
+
 variable "default_region" {
   description = "default Azure region where resources are created"
   default     = "westeurope"
